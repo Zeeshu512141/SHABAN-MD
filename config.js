735590f72e7638c849a32286a8608fee06d6a8dc
Zeeshu512141 <queenmirha09@gmail.com>
2025-08-06 15:25:14 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~yMNGyRhQ#AXfAKvWiWkF_ZdIjqCwKH8K29QytISxVgDStOkgM7m0",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~XAsilYrC#YaaqlCAYIX2N9g9HET8v-p3zd26oMrhcIy-TeNKOooM",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -23,13 +23,13 @@ PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://i.ibb.co/j9G5tmNM/shaban-md.jpg",
 // add custom menu and mention reply image url   
-BOT_NAME: process.env.BOT_NAME || "RAJPOOT GIRL",
+BOT_NAME: process.env.BOT_NAME || "Naina",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "SHABAN-MD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💔,🇵🇰",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "❤️,🇵🇰",
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
